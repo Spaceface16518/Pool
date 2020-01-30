b12acbb6fff0acf9ce77660bbb80047ac3299006
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2020-01-30 12:45:56 -0600

    Add the particle component

--- src/particles/depth/mod.rs
+++
@@ -1,8 +0,0 @@
-use rand::Rng;
-
-//pub mod depth_system;
-//pub mod depth_variation;
-
-pub fn random_depth(rng: &mut (impl Rng + ?Sized)) -> f32 {
-    rng.gen()
-}
--- src/particles/mod.rs
+++ src/particles/mod.rs
@@ -1,1 +1,14 @@
-pub mod depth;
+use amethyst::ecs::Component;
+use amethyst::ecs::VecStorage;
+use amethyst::prelude::World;
+use rand::Rng;
+
+pub struct Particle;
+
+impl Component for Particle {
+    type Storage = VecStorage<Self>;
+}
+
+pub fn generate_particles(rng: &mut (impl Rng + ?Sized)) {
+    unimplemented!()
+}
