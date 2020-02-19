5d6fd713548ac9424e7a6b5c90e00db58fe5cb09
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2020-02-19 12:32:31 -0600

    Temporarily add packed sprite sheets
    Sorting out build/packing issues

--- build.rs
+++ build.rs
@@ -1,32 +1,32 @@
 use std::{convert::TryInto, error::Error, fs::File, io::Write};
 
-use image::{ImageError, Pixel, Rgba, RgbaImage};
+use image::{ImageError, Pixel};
 use sheep::{AmethystFormat, InputSprite, SimplePacker};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let particle = particle_sprite()?;
-
-    let results =
-        sheep::pack::<SimplePacker>(vec![particle], Rgba::<u8>::CHANNEL_COUNT.try_into()?, ());
-    assert_eq!(results.len(), 1);
-    let sprite_sheet = results.into_iter().next().unwrap();
-
-    let meta = sheep::encode::<AmethystFormat>(&sprite_sheet, ());
-
-    let outbuf = RgbaImage::from_vec(
-        sprite_sheet.dimensions.0,
-        sprite_sheet.dimensions.1,
-        sprite_sheet.bytes,
-    )
-    .ok_or_else(|| "Could not construct sprite sheet from bytes".to_owned())?;
-
-    outbuf.save("resources/sprite_sheet.png")?;
-
-    let mut meta_file = File::create("resources/sprite_sheet.ron")?;
-    let meta_str = ron::ser::to_string(&meta)?;
-
-    meta_file.write_all(meta_str.as_bytes())?;
-
+    // let particle = particle_sprite()?;
+    //
+    // let results =
+    //     sheep::pack::<SimplePacker>(vec![particle], Rgba::<u8>::CHANNEL_COUNT.try_into()?, ());
+    // assert_eq!(results.len(), 1);
+    // let sprite_sheet = results.into_iter().next().unwrap();
+    //
+    // let meta = sheep::encode::<AmethystFormat>(&sprite_sheet, ());
+    //
+    // let outbuf = RgbaImage::from_vec(
+    //     sprite_sheet.dimensions.0,
+    //     sprite_sheet.dimensions.1,
+    //     sprite_sheet.bytes,
+    // )
+    // .ok_or_else(|| "Could not construct sprite sheet from bytes".to_owned())?;
+    //
+    // outbuf.save("resources/sprite_sheet.png")?;
+    //
+    // let mut meta_file = File::create("resources/sprite_sheet.ron")?;
+    // let meta_str = ron::ser::to_string(&meta)?;
+    //
+    // meta_file.write_all(meta_str.as_bytes())?;
+    //
     Ok(())
 }
 
